fa933c8c072b1f94f9a19b75f656e28bf46265d7
Daniele Milana <acidinucleici@gmail.com>
2021-10-31 15:24:32 +0000

    Initial commit from PHPSandbox

---
+++ home.php
@@ -0,0 +1,12 @@
+<?php require_once('vendor/autoload.php'); require_once('vendor/autoload.php');
+//.src/My/Classe.php
+
+namespace Src\My;
+
+class Classe {
+    function __construct()
+    {  
+        print_r('Hooooray!');        
+    }
+}
+
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+//.src/My/Classe.php
+
+namespace Src\My;
+
+class Classe {
+    function __construct()
+    {  
+        print_r('Hooooray!');        
+    }
+}
